2d67aa5f20fdc972391a718c6b62231cb5b8a38d
leoworths@gmail.com <leoworths@gmail.com>
2024-11-04 15:54:18 +0100

    added jenkinsfile

--- jenkins-server/jenkins-script.sh
+++ jenkins-server/jenkins-script.sh
@@ -1,65 +1,101 @@
 #!/bin/bash
 
-# Install AWS CLI
-sudo apt install unzip -y
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-#install docker 
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+#install java 17 headless
+
+sudo apt install openjdk-17-jre-headless -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+    echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker ubuntu #avoid permission error(adding ubuntu user to docker group)
 sudo newgrp docker #avoid restarting the server
 sudo usermod -aG docker jenkins #adding jenkins user to docker group
 sudo newgrp docker #avoid restarting the server
+sudo systemctl restart jenkins
 sudo systemctl restart docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
 
-#install sonarqube to run on docker
-sudo docker run -d --name sonarqube -p 9000:9000 sonarqube:lts-community
+#install git
+sudo apt install git -y
 
-# Install Trivy
-sudo apt-get install -y wget apt-transport-https gnupg
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update -y
+#install awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+sudo unzip awscliv2.zip
+sudo ./aws/install
+
+#install kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+
+#install eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+sudo tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+#aws eks config
+aws eks update-kubeconfig --region us-east-1 --name eks-cluster
+
+#install nodejs
+sudo apt install nodejs -y
+sudo apt install npm -y
+
+#install trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
 sudo apt-get install trivy -y
 
-# Install Java 17
-# REF: https://www.rosehosting.com/blog/how-to-install-java-17-lts-on-ubuntu-20-04/
-sudo apt update -y
-sudo apt install openjdk-17-jdk openjdk-17-jre -y
-java -version
 
-# Install Jenkins
-# REF: https://www.jenkins.io/doc/book/installing/linux/#debianubuntu
+#install sonarqube to run on docker
+sudo docker run -d --name sonarqube -p 9000:9000 sonarqube:lts-community
+#get sonarqube public ip
+echo "Sonarqube url is http://$(curl -s ifconfig.me):9000"
+#get sonarqube username and password
+echo "Sonarqube username is admin"
+echo "Sonarqube password is admin"
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+#get jenkins public ip
+echo "Jenkins url is http://$(curl -s ifconfig.me):8080"
+#get jenkins username and password
+echo "Jenkins username is admin"
+echo "Jenkins password is $(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)"
 
-sudo apt update -y                                   # to update package
-sudo apt install jenkins -y                          # to install jenkins
-
-sudo systemctl start jenkins                         # to start jenkins service
-# sudo systemctl status jenkins                        # to check the status if jenkins is running or not
-
-# Get Jenkins_Public_IP
-ip=$(curl ifconfig.me)
-port1=8080
-port2=9000
-
-# Generate Jenkins initial login password
-pass=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
-
-echo "Access Jenkins Server here --> http://$ip:$port1"
-echo "Jenkins Initial Password: $pass"
-echo
-echo "Access SonarQube Server here --> http://$ip:$port2"
-echo "SonarQube Username & Password: admin"
